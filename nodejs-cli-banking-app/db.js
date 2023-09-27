0b032810926d8e5fb6edc0c81a83d678994205e5
chibokamc <chibokachinedum@gmail.com>
2023-09-27 23:09:09 +0100

    Added a Create Account Feature

--- nodejs-cli-banking-app/db.js
+++ nodejs-cli-banking-app/db.js
@@ -1,9 +1,10 @@
-const {Client} = require("pg");
+const { Client } = require("pg");
 
 const client = new Client({
 	host: "localhost",
-	user:"postgres",
+	user: "postgres",
 	password: "nedumpostgres",
+	database: "banking_app",
 	port: 5432,
 });
 
@@ -16,3 +17,50 @@ try {
 	console.log("Error In Connectivity");
 	console.log(err);
 }
+
+const createNewAccount = async (acId, acNm, acBal) => {
+	try {
+		await client.query(
+			`insert into account ("acct_id", "acct_num", "acct_bal") values ($1,$2,$3)`,
+			[acId, acNm, acBal]
+		);
+		return true;
+	} catch (error) {
+		console.error(error.stack);
+		return false;
+	} finally {
+		await client.end();
+	}
+};
+
+const withdraw = async(acId)=>{
+  const {rows} = await client.query(`select acct_bal from account where acct_id = $1`, [acId])
+  const acBal = rows[0].acct_bal
+  console.log(`Your current balance is ${acBal}`)
+}
+
+withdraw(2)
+
+// const fetchUsers = async () => {
+// 	try {
+// 		await client.connect(); // gets connection
+// 		const { rows } = await client.query('SELECT * FROM "users"'); // sends queries
+// 		console.log(rows);
+// 	} catch (error) {
+// 		console.error(error.stack);
+// 	} finally {
+// 		await client.end(); // closes connection
+// 	}
+// };
+
+// fetchUsers();
+
+
+createNewAccount(2, "abc", 800).then((result) => {
+	if (result) {
+		console.log("New Account Created Successfully");
+	} else {
+		console.log("Account creation failed!!!");
+	}
+});
+
